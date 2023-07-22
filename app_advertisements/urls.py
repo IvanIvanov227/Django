731b4646bc7c4d4b68ffa750b315c7d1ddcf0856
Alexander <alexandersivkov228@gmail.com>
2023-07-22 17:46:12 +0500

    ДЗ пятого урока

--- app_advertisements/urls.py
+++ app_advertisements/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import index, test, test2
+from .views import *
 
 urlpatterns = [
-    path('test2/', test2, name='test2'),
-    path('', test, name='test'),
-    #path('', index),
+    #path('test2/', test2, name='test2'),
+    #path('test/', test, name='test'),
+    path('', index, name='index'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', advertisement_post, name='advertisement-post'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('profile/', profile, name='profile'),
+    path('exit_/', exit_, name='#'),
 ]
--- app_advertisements/views.py
+++ app_advertisements/views.py
@@ -8,6 +8,30 @@ def index(requests):
     return render(requests, 'index.html')
 
 
+def top_sellers(requests):
+    return render(requests, 'top-sellers.html')
+
+
+def advertisement_post(requests):
+    return render(requests, 'advertisement-post.html')
+
+
+def register(requests):
+    return render(requests, 'register.html')
+
+
+def login(requests):
+    return render(requests, 'login.html')
+
+
+def profile(requests):
+    return render(requests, 'profile.html')
+
+
+def exit_(requests):
+    return render(requests, 'index.html')
+
+
 def test(requests):
     return render(requests, 'test.html')
 
